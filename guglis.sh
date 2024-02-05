7e3c6353b768b85c16a730348ba703f42829ea8f
Gusteivon007 <159018738+Gusteivon007@users.noreply.github.com>
2024-02-05 10:16:07 -0300

    Add files via upload

---
+++ guglis.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "ola" 
+
+WARNING: apt does not have a stable CLI interface. Use with caution in scripts.
+
+Lendo listas de pacotes...
+Construindo árvore de dependências...
+Lendo informação de estado...
+Calculando atualização...
+Obtenha mais atualizações de segurança através do Ubuntu Pro com 'esm-apps' habilitado:
+  libjs-jquery-ui php-twig
+Saiba mais sobre o Ubuntu Pro em https://ubuntu.com/pro
+Os pacotes a seguir serão mantidos em suas versões atuais:
+  gjs libgjs0g
+0 pacotes atualizados, 0 pacotes novos instalados, 0 a serem removidos e 2 não atualizados.
+
+WARNING: apt does not have a stable CLI interface. Use with caution in scripts.
+
+Lendo listas de pacotes...
+Construindo árvore de dependências...
+Lendo informação de estado...
+Calculando atualização...
+Obtenha mais atualizações de segurança através do Ubuntu Pro com 'esm-apps' habilitado:
+  libjs-jquery-ui php-twig
+Saiba mais sobre o Ubuntu Pro em https://ubuntu.com/pro
+Os pacotes a seguir serão mantidos em suas versões atuais:
+  gjs libgjs0g
+0 pacotes atualizados, 0 pacotes novos instalados, 0 a serem removidos e 2 não atualizados.
+
